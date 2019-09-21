86ed2d71cbcbfe43c308c408a70752a79b32ae9d
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-09-21 14:51:23 +0800

    Add TCGA cibersort and MHCII GSVA score

--- code/tidy_cibersort.R
+++ code/tidy_cibersort.R
@@ -1,10 +1,17 @@
 # CIBERSORT result for TCGA patients
 # were obtained from TCIA <https://tcia.at/home>
-library(tidyverse)
-df = data.table::fread("/Volumes/data/biodata/TCGA/TCGA-all-cellTypeFractionsAll-20180702.tsv")
-cibersort = df %>% 
-    as_tibble() %>% 
-    select(-quanTIseq_lsei_TIL10) %>% 
-    filter(!is.na(cibersort_LM22))
 
-save(cibersort, file = "data/TCGA_cibersort.RData")
+# library(tidyverse)
+# df = data.table::fread("/Volumes/data/biodata/TCGA/TCGA-all-cellTypeFractionsAll-20180702.tsv")
+# cibersort = df %>% 
+#     as_tibble() %>% 
+#     select(-quanTIseq_lsei_TIL10) %>% 
+#     filter(!is.na(cibersort_LM22))
+# 
+# save(cibersort, file = "data/TCGA_cibersort.RData")
+
+# This data only contains 6 cell types
+# We also obtained cibersort result from TCGA publications https://gdc.cancer.gov/about-data/publications/panimmune
+cibersort = data.table::fread("data/TCGA.Kallisto.fullIDs.cibersort.relative.tsv.gz")
+# This data does not need clean, just read it into R
+
