64838d161e3c3b7f65c6fd8f60d94418d873b42f
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-09-22 19:37:40 +0800

    cache update

--- update_site.sh
+++ update_site.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -f report/comp_profile.png report/report_main.html docs/
-cp -rf report/report_main_files/ docs/report_main_files
-mv docs/report_main.html docs/index.html
+mv -f report/comp_profile.png report/report_main.html docs/
+mv -f report/report_main_files/ docs/report_main_files
+mv report/report_main.html docs/index.html
 
