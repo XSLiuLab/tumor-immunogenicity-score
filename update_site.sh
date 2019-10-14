c4dc1dc7395a79c75af19937b06629f78e4e5b86
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-10-14 18:45:38 +0800

    remove cache

--- update_site.sh
+++ update_site.sh
@@ -2,6 +2,6 @@
 
 mv -f report/comp_profile.png docs/
 mv -f report/report_main_files/ docs/
-mv -f report/report_main_cache/ docs/
+#mv -f report/report_main_cache/ docs/
 mv -f report/report_main.html docs/
 mv docs/report_main.html docs/index.html
